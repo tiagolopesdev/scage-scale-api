933860904e85986a76e4727d06338ce4e12cd913
Tiago Lopes <saxtiago14@gmail.com>
2023-06-13 18:03:42 -0300

    Feat: object to response created

---
+++ SCAGEScale/SCAGEScale.Application/Utils/RequestReponse.cs
@@ -0,0 +1,25 @@
+﻿
+
+namespace SCAGEScale.Application.Utils
+{
+    public class RequestResponse
+    {
+        public string Message { get; set; }
+        public object Data { get; set; }
+
+        public static string Error(string message)
+        {
+            return message;
+        }
+
+        public static RequestResponse New(string message, object data)
+        {
+            var response = new RequestResponse
+            {
+                Message = message,
+                Data = data
+            };
+            return response;
+        }
+    }
+}
