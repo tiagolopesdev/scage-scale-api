0ea120a56c244ad8f0210e7651a71de1d178e393
Tiago Lopes <saxtiago14@gmail.com>
2023-06-13 18:00:58 -0300

    Feat: endpoint to preview scale month

---
+++ SCAGEScale/SCAGEScale.Api/Controllers/ScaleController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SCAGEScale.Application.DTO;
+using SCAGEScale.Application.ServiceSide;
+using SCAGEScale.Application.Utils;
+using System.Net;
+
+namespace SCAGEScale.Api.Controllers
+{
+    public class ScaleController : ControllerBase
+    {
+        private readonly IScaleService _scaleService;
+        public ScaleController(IScaleService scaleService) 
+        {
+            _scaleService = scaleService;
+        }
+
+        [HttpPost("generationScale")]
+        [Produces("application/json")]
+        [ProducesResponseType(typeof(RequestResponse), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<RequestResponse>> PreviewScale([FromBody] PreviewDto previewDto)
+        {
+            try
+            {
+                var response = await _scaleService.PreviewScale(previewDto);
+
+                return Ok(RequestResponse.New("Pré-visualização da escala gerada", response));
+
+            }catch (Exception ex)
+            {
+                return BadRequest(RequestResponse.Error(ex.Message));
+            }
+        }
+    }
+}
