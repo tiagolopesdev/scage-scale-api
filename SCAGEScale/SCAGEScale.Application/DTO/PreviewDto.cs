0986cc0576011a1bd76df3d0bcd6ecac6b8c7614
Tiago Lopes <saxtiago14@gmail.com>
2023-06-13 18:03:05 -0300

    Feat: VO scale day and DTO created's

---
+++ SCAGEScale/SCAGEScale.Application/DTO/PreviewDto.cs
@@ -0,0 +1,9 @@
+﻿
+namespace SCAGEScale.Application.DTO
+{
+    public class PreviewDto
+    {
+        public List<Guid> users { get; set; }
+        public List<string> days { get; set; }
+    }
+}
---
+++ SCAGEScale/SCAGEScale.Application/VO/ScaleDay.cs
@@ -0,0 +1,11 @@
+﻿
+namespace SCAGEScale.Application.VO
+{
+    public class ScaleDay
+    {
+        public Guid CameraOne { get; set; }
+        public Guid CameraTwo { get; set; }
+        public Guid CutDesk { get; set; }
+
+    }
+}
