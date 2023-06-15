ec049a571f8c95957646ae7f32f540e00e159efc
Tiago Lopes <saxtiago14@gmail.com>
2023-06-14 22:09:10 -0300

    Feat: mthod to get properties of the user

---
+++ SCAGEScale/SCAGEScale.Application/QuerySide/IScaleQuery.cs
@@ -0,0 +1,11 @@
+﻿
+using SCAGEScale.Application.VO;
+
+namespace SCAGEScale.Application.QuerySide
+{
+    public interface IScaleQuery
+    {
+        public Task<ReferencyUser> GetUserByReferency(Guid userId);
+        public Task<List<ScaleMonth>> ScaleMonthMakedList(List<ScaleDay> scaleDays);
+    }
+}
---
+++ SCAGEScale/SCAGEScale.Infrastructure/Queries/ScaleQueries.cs
@@ -0,0 +1,61 @@
+﻿using Dapper;
+using Microsoft.Extensions.Configuration;
+using MySqlConnector;
+using SCAGEScale.Application.QuerySide;
+using SCAGEScale.Application.VO;
+
+namespace SCAGEScale.Infrastructure.Queries
+{
+    public class ScaleQueries : IScaleQuery
+    {
+        private readonly IConfiguration _configuration;
+        private string ConnectionString { get { return _configuration.GetConnectionString("DefaultConnection"); } }
+
+        public ScaleQueries(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public async Task<List<ScaleMonth>> ScaleMonthMakedList(List<ScaleDay> scaleDays)
+        {
+            var scaleMonthList = new List<ScaleMonth>();
+
+            foreach (var scaleDay in scaleDays)
+            {
+
+                var userOne = await GetUserByReferency(scaleDay.CameraOne);
+                var userTwo = await GetUserByReferency(scaleDay.CameraTwo);
+                var userThree = await GetUserByReferency(scaleDay.CutDesk);
+
+                scaleMonthList.Add(new ScaleMonth(userOne, userTwo, userThree));
+            }
+
+            return scaleMonthList;
+        }
+
+        public async Task<ReferencyUser> GetUserByReferency(Guid userId)
+        {
+            using (var connection = new MySqlConnection(ConnectionString))
+            {
+                try
+                {
+                    var response = await connection.QueryAsync<ReferencyUser>(
+                        "SELECT " +
+                            "u.id as Id, " +
+                            "u.name as Name, " +
+                            "u.email as Email, " +
+                            "u.sex as Sex " +
+                        "FROM users as u " +
+                        "WHERE isEnable = 1 AND u.id = @id;", new { id = userId });
+
+                    if (response.Count() == 0) return null;
+
+                    return response.ToList().FirstOrDefault();
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+        }
+    }
+}
