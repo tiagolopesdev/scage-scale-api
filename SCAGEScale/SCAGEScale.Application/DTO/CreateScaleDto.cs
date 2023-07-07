32553854b08846ae2e73f5e3811adb6f622836e8
Tiago Lopes <saxtiago14@gmail.com>
2023-07-07 15:36:35 -0300

    Feat: DTO's created to use in controller

---
+++ SCAGEScale/SCAGEScale.Application/DTO/CreateScaleDto.cs
@@ -0,0 +1,9 @@
+﻿
+namespace SCAGEScale.Application.DTO
+{
+    public class CreateScaleDto
+    {
+        public DayDto Day { get; set; }
+        public ScaleMonthDto Peoples { get; set; }
+    }
+}
---
+++ SCAGEScale/SCAGEScale.Application/DTO/DayDto.cs
@@ -0,0 +1,9 @@
+﻿
+namespace SCAGEScale.Application.DTO
+{
+    public class DayDto
+    {
+        public string Name { get; set; }
+        public DateTime DateTime { get; set; }
+    }
+}
