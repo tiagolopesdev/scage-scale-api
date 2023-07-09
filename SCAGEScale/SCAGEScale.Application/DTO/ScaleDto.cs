bd8eb9e5490395ec6c37a6ceac26d4a087784531
Tiago Lopes <saxtiago14@gmail.com>
2023-07-09 18:14:35 -0300

    Feat: new DTO to return data scales

---
+++ SCAGEScale/SCAGEScale.Application/DTO/ScaleDto.cs
@@ -0,0 +1,15 @@
+﻿
+using SCAGEScale.Application.VO;
+
+namespace SCAGEScale.Application.DTO
+{
+    public class ScaleDto
+    {
+        public string Name { get; set; }
+        public int Transmissions { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+        public StatusScaleVo Status { get; set; }
+        public List<DayDto> Days { get; set; }
+    }
+}
